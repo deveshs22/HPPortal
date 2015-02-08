a8c3725b1a17111ea4cee3d0dcbdb24d0f1364b5
dhruvjoshi04 <dhruv.anderson@gmail.com>
2015-02-08 17:04:48 +0530

    add files

---
+++ HPPortal.Web/Users/AddUser.aspx
@@ -0,0 +1,113 @@
+﻿<%@ Page Title="UserInsert" Language="C#" MasterPageFile="~/Site.Master" EnableViewState="true" CodeBehind="AddUser.aspx.cs" Inherits="HPPortal.Web.Users.AddUser" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="MainContent">
+    <div class="row">
+        <h2>Insert User</h2>
+        <div>
+            <p>&nbsp;</p>
+            <fieldset class="form-horizontal">
+                <div class="form-group">
+                    <span class="col-sm-2 control-label">Email Address</span>
+                    <div class="col-sm-3">
+                        <asp:TextBox ID="txtEmail" CssClass="form-control DDTextBox" ValidateRequestMode="Enabled" runat="server" />
+                        <asp:RequiredFieldValidator ID="emailReq" runat="server" CssClass="validation" ErrorMessage="Required" ControlToValidate="txtEmail"></asp:RequiredFieldValidator>
+                        <asp:RegularExpressionValidator ID="RegularExpressionValidator2"
+                            runat="server" ErrorMessage="Please Enter Valid Email ID"
+                            ControlToValidate="txtEmail"
+                            CssClass="validation"
+                            ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*">
+                        </asp:RegularExpressionValidator>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <span class="col-sm-2 control-label">Password</span>
+                    <div class="col-sm-3">
+                        <asp:TextBox ID="txtPassword" TextMode="Password" CssClass="form-control DDTextBox" ValidateRequestMode="Enabled" runat="server" />
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" CssClass="validation" runat="server" ErrorMessage="Required" ControlToValidate="txtPassword"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <span class="col-sm-2 control-label">Name</span>
+                    <div class="col-sm-3">
+                        <asp:TextBox ID="txtName" CssClass="form-control DDTextBox" runat="server" />
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" CssClass="validation" runat="server" ErrorMessage="Required" ControlToValidate="txtName"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <span class="col-sm-2 control-label">Mobile</span>
+                    <div class="col-sm-3">
+                        <asp:TextBox ID="txtPhone" CssClass="form-control DDTextBox" runat="server" />
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" CssClass="validation" runat="server" ErrorMessage="Required" ControlToValidate="txtPhone"></asp:RequiredFieldValidator>
+                        <asp:RegularExpressionValidator ID="RegularExpressionValidator1"
+                            ControlToValidate="txtPhone"
+                            ValidationExpression="\d{10}"
+                            ErrorMessage="Mobile number must be 10 numeric digits"
+                            EnableClientScript="False"
+                            CssClass="validation"
+                            runat="server" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <span class="col-sm-2 control-label">Roles</span>
+                    <div class="col-sm-3">
+                        <asp:DropDownList ID="ddlRoles" CssClass="form-control" runat="server"></asp:DropDownList>
+
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <span class="col-sm-2 control-label">Reporting</span>
+                    <div class="col-sm-3">
+                        <asp:DropDownList ID="ddlReporting" CssClass="form-control" OnSelectedIndexChanged="ddlReporting_SelectedIndexChanged" AutoPostBack="true" runat="server"></asp:DropDownList>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <h3>Select City</h3>
+                    <div class="col-sm-3">
+                        <asp:TreeView ID="treeViewCity" ShowCheckBoxes="All" runat="server">
+                        </asp:TreeView>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <asp:Button runat="server" ID="InsertButton" OnClick="btnInsertClick" Text="Save" CssClass="btn btn-primary" />
+                        <asp:Button runat="server" ID="CancelButton" OnClick="btnCancelClick" Text="Cancel" CausesValidation="false" CssClass="btn btn-default" />
+                    </div>
+                </div>
+            </fieldset>
+
+        </div>
+
+
+    </div>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("[id*=treeViewCity] input[type=checkbox]").bind("click", function () {
+                var table = $(this).closest("table");
+                if (table.next().length > 0 && table.next()[0].tagName == "DIV") {
+                    //Is Parent CheckBox
+                    var childDiv = table.next();
+                    var isChecked = $(this).is(":checked");
+                    $("input[type=checkbox]", childDiv).each(function () {
+                        if (isChecked) {
+                            $(this).attr("checked", "checked");
+                        } else {
+                            $(this).removeAttr("checked");
+                        }
+                    });
+                } else {
+                    //Is Child CheckBox
+                    var parentDIV = $(this).closest("DIV");
+                    if ($("input[type=checkbox]", parentDIV).length == $("input[type=checkbox]:checked", parentDIV).length) {
+                        $("input[type=checkbox]", parentDIV.prev()).attr("checked", "checked");
+                    } else {
+                        $("input[type=checkbox]", parentDIV.prev()).removeAttr("checked");
+                    }
+                }
+            });
+        })
+
+    </script>
+</asp:Content>
---
+++ HPPortal.Web/Users/AddUser.aspx.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+using HPPortal.Data.Models;
+
+
+namespace HPPortal.Web.Users
+{
+    public partial class AddUser : System.Web.UI.Page
+    {
+        protected HPPortal.Data.Models.HPSiteDBContext _db = new HPPortal.Data.Models.HPSiteDBContext();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                int userId;
+                IsNew = true;
+                if (int.TryParse(Request.Path.Split('/').Last(), out userId))
+                {
+                    IsNew = false;
+                    var item = _db.Users.Find(userId);
+                    User = item;
+                    FillData();
+                }
+
+                FillRoles();
+                FillReportingManagers();
+            }
+        }
+
+        private void FillData()
+        {
+            txtEmail.Text = User.EmailId;
+            txtName.Text = User.Name;
+            txtPassword.Text = User.Pwd;
+            txtPhone.Text = User.Mobile;
+        }
+
+        protected void btnInsertClick(object sender, EventArgs e)
+        {
+            using (_db)
+            {
+                if (IsNew)
+                {
+                    var item = new HPPortal.Data.Models.User();
+                    CommitToItem(item);
+
+                    if (ModelState.IsValid)
+                    {
+                        _db.Users.Add(item);
+                        _db.SaveChanges();
+                        Response.Redirect("Default");
+                    }
+                }
+                else
+                {
+                    CommitToItem(User);
+
+                    if (ModelState.IsValid)
+                    {
+                        _db.SaveChanges();
+                        Response.Redirect("../Default");
+                    }
+                }
+            }
+        }
+
+        private void CommitToItem(Data.Models.User item)
+        {
+            item.EmailId = txtEmail.Text.Trim();
+            item.Name = txtName.Text.Trim();
+            item.Pwd = txtPassword.Text.Trim();
+            item.RoleId = Convert.ToInt32(ddlRoles.SelectedValue);
+            item.Role = _db.Roles.Find(Convert.ToInt32(ddlRoles.SelectedValue));
+            item.ReportingId = Convert.ToInt32(ddlReporting.SelectedValue);
+            item.User2 = _db.Users.Find(Convert.ToInt32(ddlReporting.SelectedValue));
+            item.Mobile = txtPhone.Text.Trim();
+            item.Active = true;
+            item.Cities = GetSelectedCities();
+        }
+
+        protected void btnCancelClick(object sender, EventArgs e)
+        {
+            if (IsNew)
+                Response.Redirect("Default");
+            else
+                Response.Redirect("../Default");
+        }
+
+        private void FillRoles()
+        {
+            var roles = _db.Roles.ToList();
+            ddlRoles.DataSource = roles;
+            ddlRoles.DataTextField = "Description";
+            ddlRoles.DataValueField = "RoleId";
+            ddlRoles.DataBind();
+            if (!IsNew)
+                ddlRoles.SelectedValue = User.RoleId.ToString();
+        }
+
+        private void FillReportingManagers()
+        {
+            var cities = GetSelectedCities();
+
+            var roles = _db.Users.ToList();
+            if (!IsNew)
+            {
+                var role = roles.FirstOrDefault(r => r.UserId == User.UserId);
+                roles.Remove(role);
+            }
+
+            ddlReporting.DataSource = roles;
+            ddlReporting.DataTextField = "Name";
+            ddlReporting.DataValueField = "UserId";
+            ddlReporting.DataBind();
+            var reportingId = roles.FirstOrDefault().UserId.ToString();
+            if (!IsNew)
+            {
+                ddlReporting.SelectedValue = User.ReportingId.ToString();
+                reportingId = User.ReportingId.ToString();
+            }
+
+            FillTreeView(reportingId);
+        }
+
+        private void FillTreeView(string reportingId)
+        {
+            var items = _db.Users.Include(u => u.Cities).ToList();
+            var item = items.FirstOrDefault(i => i.UserId == Convert.ToInt32(reportingId));
+            var zones = _db.Zones.Include(z => z.Cities).ToList();
+            var zonesToBind = new List<Zone>();
+
+            foreach (var zone in zones)
+            {
+                var data = zone.Cities.Where(t2 => item.Cities.Any(t1 => t1.CityId.Equals(t2.CityId)));
+                if (data != null && data.ToList().Count > 0)
+                    zonesToBind.Add(zone);
+            }
+
+            BindDataInTreeView(zonesToBind);
+        }
+
+        private void BindDataInTreeView(List<Zone> zonesToBind)
+        {
+            if (treeViewCity.Nodes.Count > 0)
+                treeViewCity.Nodes.Clear();
+
+            var count = 0;
+            foreach (var zone in zonesToBind)
+            {
+                TreeNode node = new TreeNode
+                {
+                    Value = zone.ZoneId.ToString(),
+                    Text = zone.Description
+                };
+
+                treeViewCity.Nodes.Add(node);
+
+                foreach (var city in zone.Cities)
+                {
+                    TreeNode child = new TreeNode
+                    {
+                        Value = city.CityId.ToString(),
+                        Text = city.Description
+                    };
+                    treeViewCity.Nodes[count].ChildNodes.Add(child);
+
+                    if (User != null && User.Cities.Any(c => c.CityId == city.CityId))
+                    {
+                        child.Checked = true;
+                    }
+                }
+                count++;
+            }
+        }
+
+        private List<City> GetSelectedCities()
+        {
+            List<City> cities = new List<City>();
+            foreach (TreeNode node in treeViewCity.Nodes)
+            {
+                if (node.ChildNodes.Count > 0)
+                {
+                    foreach (TreeNode child in node.ChildNodes)
+                    {
+                        if (child.Checked)
+                        {
+                            var city = _db.Cities.Find(Convert.ToInt32(child.Value));
+                            cities.Add(city);
+                        }
+                    }
+                }
+            }
+            return cities;
+        }
+
+        protected void ddlReporting_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillTreeView(ddlReporting.SelectedValue);
+        }
+
+        private User User
+        {
+            get
+            {
+                return ViewState["user"] as User;
+            }
+            set
+            {
+                ViewState["user"] = value;
+            }
+        }
+
+        private bool IsNew
+        {
+            get
+            {
+                return (bool)ViewState["isNew"];
+            }
+            set
+            {
+                ViewState["isNew"] = value;
+            }
+        }
+    }
+}
