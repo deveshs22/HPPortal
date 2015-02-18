ddf5992850fded8dca5c2ed2bf87dabf0c86b6c1
deveshs22 <dev.sinha22@gmail.com>
2015-02-18 23:56:11 +0530

    SessionData added

---
+++ HPPortal.Web/Utility/SessionData.cs
@@ -0,0 +1,34 @@
+﻿using HPPortal.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HPPortal.Web
+{
+    public class SessionData
+    {
+        private SessionData()
+        {
+            
+        }
+
+        public static SessionData Current
+        {
+           get
+            {
+                SessionData session = (SessionData)HttpContext.Current.Session["__Session__"];
+                if (session == null)
+                {
+                    session = new SessionData();
+                    HttpContext.Current.Session["__Session__"] = session;
+                }
+                return session;
+            }
+        }
+
+        public int UserId { get; set; }
+        public int PartnerId { get; set; }
+        public string QuarterYear { get; set; }
+    }
+}
